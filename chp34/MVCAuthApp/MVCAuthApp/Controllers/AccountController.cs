c3db6d7c19a55ff69c9a61325dc7179c3fb91c5a
Chucks Nwachukwu <chucks@bbd.co.za>
2020-03-10 02:11:55 +0200

    MVC Authentication

---
+++ chp34/MVCAuthApp/MVCAuthApp/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Security;
+using System.Web.Mvc;
+using MVCAuthApp.Models.Views;
+
+namespace MVCAuthApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            LoginViewModel loginView = new LoginViewModel();
+            return View(loginView);
+           
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel creds, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                /* FormsAuthetication.Authenticate is obsolete. Use Memebership.ValidateUser instead */
+                if (FormsAuthentication.Authenticate(creds.Username, creds.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(creds.Username, false); // The second paramater must be true if you want the cookie to persist between sessions
+                    return Redirect(returnUrl ?? Url.Action("Index", "Home"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username and/or password");
+                }
+                
+            }
+         
+            return View(creds);
+        }
+    }
+}
---
+++ chp34/MVCAuthApp/MVCAuthApp/Models/Views/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCAuthApp.Models.Views
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { set; get; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { set; get; }
+    }
+}
